d026b8187f50a52ee1cf31243661d2a108fbf942
jamesrx13 <rudasmarinjf@gmail.com>
2025-08-24 16:29:01 -0500

    Primer commit

---
+++ src/js/Main.js
@@ -0,0 +1,113 @@
+// VARIABLES
+const roles = [
+    "Desarrollador Fullstack",
+    "Desarrollador Frontend",
+    "Desarrollador Backend",
+    "Desarrollador Mobile",
+    "Diseñador Web",
+];
+
+let i = 0;      
+let j = 0;     
+let deleting = false;
+const speed = 100;   
+const pause = 1500; 
+
+// FUNCIONES
+const onScroll = () => {
+    const nav = document.querySelector("nav");
+
+    if (window.scrollY > 100) {
+
+        nav.classList.add("onScroll");
+
+        nav.addEventListener("animationend", () => {
+
+            nav.classList.remove("onScroll");
+            if(nav.querySelector("ul").classList.contains("open")) return;
+            nav.classList.add("scrolled");
+
+        });
+
+    } else if (window.scrollY <= 100) {
+
+        if (nav.classList.contains("onScroll")) {
+
+            nav.classList.remove("scrolled");
+            nav.classList.add("offScroll");
+
+            nav.addEventListener("animationend", () => {
+                nav.classList.remove("offScroll");
+                nav.classList.remove("scrolled");
+            });
+
+        }
+
+    }
+}
+
+const typeEffect = () => {
+
+    const typingEl = document.querySelector(".typing");
+
+    const current = roles[i];
+
+    if (!deleting) {
+        typingEl.textContent = current.slice(0, j++);
+        if (j > current.length) {
+            deleting = true;
+            setTimeout(typeEffect, pause);
+            return;
+        }
+    } else {
+        typingEl.textContent = current.slice(0, j--);
+        if (j < 0) {
+            deleting = false;
+            i = (i + 1) % roles.length;
+            j = 0;
+        }
+    }
+
+    setTimeout(typeEffect, deleting ? speed / 2 : speed);
+}
+
+const toggleMenu = () => {
+
+    const openMenuBtn = document.getElementById("open-menu");
+    const nav = document.querySelector("nav");
+    const menu = nav.querySelector("ul");
+
+    openMenuBtn.addEventListener("click", () => {
+        nav.classList.remove("scrolled");
+        menu.classList.add("open");
+    });
+    
+    menu.querySelectorAll("li").forEach(item => {
+
+        item.addEventListener("click", () => {
+
+            menu.classList.remove("open");
+            menu.classList.add("close");
+
+            menu.addEventListener("animationend", () => {
+                menu.classList.remove("close");
+            });
+
+            if (window.scrollY > 100) {
+                nav.classList.add("scrolled");
+                console.log('Menu add scrolled');
+            }
+        });
+
+    })
+
+}
+
+// EVENTOS
+document.addEventListener("scroll", onScroll);
+
+document.addEventListener("DOMContentLoaded", () => {
+    typeEffect();
+    toggleMenu();
+    onScroll(); // Se añade para que al recargar la página en una posición diferente a la inicial
+});
